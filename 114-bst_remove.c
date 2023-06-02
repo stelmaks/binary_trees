e3845088cdb87e53c6187da408f40e192dc650a6
stelmaks <stelmaks@101@gmail.com>
2023-06-02 09:31:55 -0700

    BST - Remove

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 bst_t *inorder_successor(bst_t *root);
+bst_t *bst_delete(bst_t *root, bst_t *node);
 bst_t *bst_remove_recursive(bst_t *root, bst_t *node, int value);
 bst_t *bst_remove(bst_t *root, int value);
-bst_t *bst_delete(bst_t *root, bst_t *node);
 
 
 /**
